d1a4dd4760289bf4123ab83df50d9292699c0321
rohitaroradung <rohit.179303118@muj.manipal.edu>
2019-03-07 20:18:52 +0530

    Merge branch 'TFragments.00-StartingCode' of C:\Users\hp\AndroidStudioProjects\Android_Me2 with conflicts.

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -1,7 +1,8 @@
 package com.example.android.android_me.ui;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,18 +12,30 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BodyPartFragment extends Fragment {
-    List<Integer> mlist = AndroidImageAssets.getHeads();
+    public static final String IMAGE_ID_LIST = "image_ids";
+    public static final String LIST_INDEX = "list_index";
+    private List<Integer> mImageIds;
+    private int mListIndex;
+    List<Integer> mlist;
     int mid;
     private static final String TAG = "BodyPartFragment";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if(savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
+        }
+
+
         // Inflate the layout for this fragment
         View rootview =  inflater.inflate(R.layout.fragment_body_part, container, false);
-        ImageView bodypart = (ImageView) rootview.findViewById(R.id.bodypart);
+       final ImageView bodypart = (ImageView) rootview.findViewById(R.id.bodypart);
+
        if(mlist!=null) {
            bodypart.setImageResource(mlist.get(mid));
        }
@@ -30,6 +43,16 @@ public class BodyPartFragment extends Fragment {
        {
            Log.v(TAG,"this has null value");
        }
+        bodypart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mid<mlist.size()-1)
+                    mid++;
+                else
+                    mid =0;
+                bodypart.setImageResource(mlist.get(mid));
+            }
+        });
         return  rootview;
 
     }
@@ -41,5 +64,12 @@ public class BodyPartFragment extends Fragment {
 
     public void setMlist(List<Integer> mlist) {
         this.mlist = mlist;
+    }
+
+    
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, (ArrayList<Integer>) mImageIds);
+        currentState.putInt(LIST_INDEX, mListIndex);
     }
 }
